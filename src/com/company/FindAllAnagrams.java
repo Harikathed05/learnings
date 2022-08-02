1cc03d6ff3b9d72483b6d9abfc497f74fceb01b9
Hari Kathed <harikath@amazon.com>
2022-08-02 16:27:08 +0530

    Update the FindAllAnagrams

--- src/com/company/FindAllAnagrams.java
+++ src/com/company/FindAllAnagrams.java
@@ -17,30 +17,28 @@ import java.util.List;
  */
 
 public class FindAllAnagrams {
-    private boolean isAnagram(String str1, String str2) {
-        if(str1.length() != str2.length()) {
-            return false;
-        }
+    private boolean isAnagram(String str1, HashMap<Character, Integer> hashMapStr2) {
         HashMap<Character, Integer> hashMapStr1 = new HashMap<>();
         for (char c:
              str1.toCharArray()) {
             hashMapStr1.put(c, hashMapStr1.getOrDefault(c, 0) + 1);
         }
-        HashMap<Character, Integer> hashMapStr2 = new HashMap<>();
-        for (char c:
-                str2.toCharArray()) {
-            hashMapStr2.put(c, hashMapStr2.getOrDefault(c, 0) + 1);
-        }
+
         return hashMapStr1.equals(hashMapStr2);
     }
 
     public int[] findAllAnagrams(String original, String p) {
         int pSize = p.length();
         List<Integer> integerList = new ArrayList<>();
+        HashMap<Character, Integer> hashMapStr2 = new HashMap<>();
+        for (char c:
+                p.toCharArray()) {
+            hashMapStr2.put(c, hashMapStr2.getOrDefault(c, 0) + 1);
+        }
         for(int i = 0; i < original.length(); i++) {
             if((i + pSize) <= original.length()) {
                 String str1 =  original.substring(i, i+pSize);
-                if(isAnagram(str1, p)) {
+                if(isAnagram(str1, hashMapStr2)) {
                     integerList.add(i);
                 }
             }
@@ -50,8 +48,8 @@ public class FindAllAnagrams {
 
     public static void main(String [] args) {
         FindAllAnagrams findAllAnagrams = new FindAllAnagrams();
-        //int[] indexes = findAllAnagrams.findAllAnagrams("cbaebabacd", "abc");
-        int[] indexes = findAllAnagrams.findAllAnagrams("abab", "ab");
+        int[] indexes = findAllAnagrams.findAllAnagrams("cbaebabacd", "abc");
+        //int[] indexes = findAllAnagrams.findAllAnagrams("abab", "ab");
         for (int i = 0; i < indexes.length; i++) {
             System.out.println(indexes[i]);
         }
